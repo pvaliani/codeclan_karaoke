236ecc01a3b3d3a51ef1a284833ae08b6fa1255b
Pedram Valiani <p.valiani@gmail.com>
2020-11-13 17:30:58 +0000

    adds all room parameters, tests room name

--- classes/room.py
+++ classes/room.py
@@ -1,5 +1,5 @@
 class Room:
-    def __init__(self, room_name, song_in_room, guests_in_room):
-        self.room_name = room_name
+    def __init__(self, name, song_in_room, guests_in_room):
+        self.name = name
         self.song_in_room = []
         self.guests_in_room = []
--- tests/room_test.py
+++ tests/room_test.py
@@ -1,7 +1,15 @@
 import unittest
-#from classes.room import Room
+from classes.room import Room
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
-        pass
+        self.room = Room("Room 1", "Beautiful Day by U2", "Pedram Valiani")
+    
+    # - This test determines that a room name exists by comparing the object self.room with attribute "name" to the value of "Room 1"
+
+    # - self.room.name reads as self.room -> the room object from classes.room and .name is the attribute of the room as defined in the Room class
+
+    def test_room_has_name(self):
+        self.assertEqual(self.room.name, "Room 1")
+        
 
