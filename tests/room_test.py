ae53c5af80348338fd9598e2eec559dfa909e067
Pedram Valiani <p.valiani@gmail.com>
2020-11-13 17:52:04 +0000

    MVP complete yeee buddy

--- tests/room_test.py
+++ tests/room_test.py
@@ -12,9 +12,12 @@ class TestRoom(unittest.TestCase):
     def test_room_has_name(self):
         self.assertEqual(self.room.name, "Room 1")
 
-    # - This test
+    # - This test checks that the room has a song by comparing the self.room instance object with the 2nd parameter of the room instance
+
     def test_room_has_song(self):
         self.assertEqual(self.room.song_in_room, "Beautiful Day by U2")
+
+    # - This test checks that the room has a guest by comparing the self.guest instance object with the 3rd parameter of the room instance
 
     def test_room_has_guest(self):
         self.assertEqual(self.room.guests_in_room, "Pedram Valiani")
