b8594d3d9ddce0137c3ff01bf9ecd7c06e642855
Pedram Valiani <p.valiani@gmail.com>
2020-11-15 19:58:02 +0000

    Tidy up

--- classes/guest.py
+++ classes/guest.py
@@ -3,11 +3,16 @@ class Guest:
         self.name = name
         self.wallet = wallet
 
+    
+    # - Method checks whether a customer can afford entry by comparing their wallet to the entry price of the room
+
     def customer_can_afford_entry(self, room):
         if self.wallet >= room.price:
             return True
         else:
             return False
     
+
+    # - Method to allow customer to pay a bill by an amount from their wallet 
     def pay_the_bill(self, amount):
         self.wallet -= amount
--- classes/room.py
+++ classes/room.py
@@ -1,4 +1,5 @@
 class Room:
+    # - Constructor - songs_in_room and guests are lists which store the playlist and the guests information
     def __init__(self, name, price, room_theme):
         self.name = name
         self.price = price
@@ -6,18 +7,27 @@ class Room:
         self.songs_in_room = []
         self.guests = []
 
+    # - Method adds a song to the playlist cue for the room
 
     def add_song_to_room(self, song_name):
         self.songs_in_room.append(song_name)
+    
+    # - Method checks in a guest to the room
 
     def check_in_guest(self, guest_name, price, wallet):
         self.guests.append(guest_name)
 
+    # - Method checks out a guest from the room
+
     def check_out_guest(self, guest_name):
         self.guests.remove(guest_name)
 
+    # - Method adds a song by its full title - i.e artist and genre, to the playlist
+
     def add_song_to_room_playlist(self, song_name):
         self.songs_in_room.append(song_name)
+
+    # - Method retrieves the length of the playlist
 
     def get_song_list_length(self):
         return len(self.songs_in_room)
