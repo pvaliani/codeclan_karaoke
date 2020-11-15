70c0025fdfb5442a3e69911fd260b5c084bae343
Pedram Valiani <p.valiani@gmail.com>
2020-11-15 21:47:56 +0000

    Extensions Tidy Up

--- tests/song_test.py
+++ tests/song_test.py
@@ -8,11 +8,12 @@ class TestSong(unittest.TestCase):
     # - This test determines that a song exists by comparing the object self.song with attribute "name" to the value of "Beautiful Day by U2"
 
     # - self.song.name reads as self.song -> the song object from classes.song and .name is the attribute of the song as defined in the Song class
-
+    
+    # - Check that the song has a title
     def test_song_has_name(self):
         self.assertEqual(self.song.name, "Beautiful Day")
 
-    # check that the song has an artist
+    # - Check that the song has an artist
 
     def test_song_has_artist(self):
         self.assertEqual(self.song.artist, "U2")
